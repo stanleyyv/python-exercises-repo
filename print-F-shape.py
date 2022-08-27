267b68b85758b4f19deeb01e9d6c6e569b42ad1a
Stanley Vuong <stanleyyv@gmail.com>
2022-08-27 17:18:53 -0500

    Adding new method for printing F shape

--- print-F-shape.py
+++ print-F-shape.py
@@ -4,6 +4,15 @@
 # XXXXX
 # XX
 # XX
+
+# Single loop with string multiplier
 nums = [5,2,5,2,2]
 for x in nums:
     print (x*"X")
+
+# Nested loop
+nums = [5,2,5,2,2]
+for num in nums:
+    for x in range(num):
+        print("x", end="")
+    print("")
