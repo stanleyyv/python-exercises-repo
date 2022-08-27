ef19810212da5aec37b67d23701ea04104c25c97
Stanley Vuong <stanleyyv@gmail.com>
2022-08-27 17:56:50 -0500

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+# Practicing with dictionary
+
+customer = {
+  "name": "Stanley Vuong",
+  "age": 29,
+  "living": True
+}
+
+#replace name with only first name
+customer["name"] = "Stanley"
+print(customer["name"])
+#return None
+print(custoemr["last name"])
